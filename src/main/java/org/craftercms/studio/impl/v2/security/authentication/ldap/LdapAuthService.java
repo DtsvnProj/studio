c4a1b2739bb51f12b04e9ed5a2fbc03a527f92b9
Nguyen Van Toan <toannv@dtsvn.net>
2025-03-12 14:52:41 +0700

    update : update authenticate ldap

--- src/main/java/org/craftercms/studio/impl/v2/security/authentication/ldap/LdapAuthService.java
+++ src/main/java/org/craftercms/studio/impl/v2/security/authentication/ldap/LdapAuthService.java
@@ -2,27 +2,65 @@ package org.craftercms.studio.impl.v2.security.authentication.ldap;
 
 import org.craftercms.studio.api.v2.dal.User;
 import org.craftercms.studio.api.v2.service.security.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.filter.AndFilter;
 import org.springframework.ldap.filter.EqualsFilter;
+import org.springframework.ldap.support.LdapUtils;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.naming.directory.DirContext;
+import javax.naming.ldap.LdapName;
+import java.util.List;
 
 public class LdapAuthService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LdapAuthService.class);
     private final LdapTemplate ldapTemplate;
     private final UserService userService;
+    private String url;
+    private String base;
+    private String userDn;
+    private String password;
+    private String pooled;
 
     public LdapAuthService(LdapTemplate ldapTemplate, UserService userService) {
         this.ldapTemplate = ldapTemplate;
         this.userService = userService;
     }
 
+    // Getter & Setter (cần thiết để Spring inject properties từ XML)
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public void setUserDn(String userDn) {
+        this.userDn = userDn;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPooled(String pooled) {
+        this.pooled = pooled;
+    }
+
     public boolean authenticate(String username, String password) {
+
+        LOGGER.info("Authenticating user query | url: {} ; base: {} ; userDn: {} ; password: {} ; pooled: {}", this.url, this.base, this.userDn, this.password, this.pooled);
+        LOGGER.info("User login: {}/{}", username, password);
         AndFilter filter = new AndFilter();
-        filter.and(new EqualsFilter("cn", username)); // Dùng cn thay vì uid
+        filter.and(new EqualsFilter("uid", username)); // Dùng cn thay vì uid
 
         try {
-            return ldapTemplate.authenticate("ou=people", filter.encode(), password);
+            return ldapTemplate.authenticate(LdapUtils.emptyLdapName(), filter.encode(), password);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -33,7 +71,7 @@ public class LdapAuthService {
         try {
             User user = userService.getUserByUsernameInternal(username);
             return user;
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new UsernameNotFoundException("User not found in DB: " + username);
         }
